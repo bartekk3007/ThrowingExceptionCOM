128ed2a206aba4f14c2c2e268f67756b624f1188
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-04-18 23:18:53 +0200

    Add files via upload

---
+++ KlientZadanie5/Klient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+using KSR_WCF1;
+
+namespace Client
+{
+    public class Client
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ChannelFactory<IZadanie1>(new NetNamedPipeBinding(), new EndpointAddress("net.pipe://localhost/ksr-wcf1-test"));
+            var client = factory.CreateChannel();
+
+            Console.WriteLine(client.Test("188701"));
+
+            try
+            {
+                client.RzucWyjatek(true);
+            }
+            catch (FaultException<Wyjatek> e)
+            {
+                Console.WriteLine(client.OtoMagia(e.Detail.magia));
+            }
+
+            ((IDisposable)client).Dispose();
+            factory.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
